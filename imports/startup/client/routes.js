142111f1904ce8060b4529844402845a221680a8
link <s3508932@student.rmit.edu.au>
2016-07-11 18:53:51 +1000

    masonry completed

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -5,7 +5,7 @@ import '../../ui/layouts/app-body.js';
 FlowRouter.route('/',{
     name: 'App.home',
     action(){
-        BlazeLayout.render('App_body', {main: "home"});
+        BlazeLayout.render('App_body', {main: "pins"});
     },
 });
 
--- imports/ui/layouts/app-body.js
+++ imports/ui/layouts/app-body.js
@@ -1,1 +1,27 @@
 import './app-body.html';
+import './pins.html';
+import { Template } from 'meteor/templating';
+
+
+
+Template.pins.helpers({
+    pins(){
+        console.log('pins');
+        return [];
+    },
+});
+
+
+Template.pins.rendered = function() {
+    var $container = $('#pins');
+    $.resize(()=>{
+        $container.masonry({
+            itemSelector : '.card',
+            columnWidth : '.card',
+            percentPosition: true,
+            isFitWidth: true,
+            resize: true,
+            percentPosition: true,
+            gutter:10,
+        })});
+};
